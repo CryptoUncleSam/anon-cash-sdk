115e782f7627e0c667d42b5452f92fd15f33c37c
stanNthe5 <st@jcow.net>
2025-11-09 09:27:23 +0800

    add e2e test for spl

---
+++ __tests__/e2espl.test.ts
@@ -0,0 +1,70 @@
+import { describe, it, expect, vi, beforeAll, beforeEach, type Mock } from "vitest";
+import dotenv from 'dotenv';
+import { PrivacyCash } from "../src";
+import { getAccount, getAssociatedTokenAddress } from "@solana/spl-token";
+import { FEE_RECIPIENT, USDC_MINT } from "../src/utils/constants";
+import { Connection } from "@solana/web3.js";
+dotenv.config();
+const TEST_AMOUNT = 2
+const units_per_token = 1_000_000
+let rpc = "https://domini-i2gp2o-fast-devnet.helius-rpc.com"
+describe('e2e test', async () => {
+    const connection = new Connection(rpc)
+    let fee_recipient_ata = await getAssociatedTokenAddress(USDC_MINT, FEE_RECIPIENT, true)
+    let feeRecipientAccount = await getAccount(connection, fee_recipient_ata)
+    let feeREcipientBalance_before = feeRecipientAccount.amount
+
+    if (!process.env.PRIVATE_KEY) {
+        throw new Error('missing PRIVATE_KEY in .env')
+    }
+    let client = new PrivacyCash({
+        RPC_url: rpc,
+        owner: process.env.PRIVATE_KEY
+    })
+    let balance_original = await client.getPrivateBalanceUSDC()
+
+    // deposit
+    await client.depositUSDC({
+        base_units: TEST_AMOUNT * units_per_token
+    })
+    let balance_after_deposit = await client.getPrivateBalanceUSDC()
+
+    // withdraw wrong amount
+    it(`show throw error if withdraw amount less than 1`, async () => {
+        await expect(client.withdrawUSDC({
+            base_units: 0.9 * units_per_token
+        })).rejects.toThrow()
+    })
+
+    // withdraw
+    let withdrawRes = await client.withdrawUSDC({
+        base_units: TEST_AMOUNT * units_per_token
+    })
+
+    await new Promise(r => setTimeout(r, 10_000));
+
+    feeRecipientAccount = await getAccount(connection, fee_recipient_ata)
+    let feeREcipientBalance_after = feeRecipientAccount.amount
+
+    let balance_after_withdraw = await client.getPrivateBalanceUSDC()
+
+    it('balance is a number', () => {
+        expect(balance_original.base_units).to.be.a('number')
+    })
+
+    it(`balance should be increased ${TEST_AMOUNT} USDC`, () => {
+        expect(balance_after_deposit.base_units).equal(balance_after_withdraw.base_units + TEST_AMOUNT * units_per_token)
+    })
+
+    it('should keep balance unchanged after depositing and withdrawing the same amount', () => {
+        expect(balance_original.base_units).equal(balance_after_withdraw.base_units)
+    })
+
+    it(`withdraw real amount plus fee should  be ${TEST_AMOUNT * units_per_token}`, () => {
+        expect(withdrawRes.base_units + withdrawRes.fee_base_units).equal(TEST_AMOUNT * units_per_token)
+    })
+
+    it('fee recipient amount should be inceased by the withdraw fee', () => {
+        expect(withdrawRes.fee_base_units).equal(Number(feeREcipientBalance_after - feeREcipientBalance_before))
+    })
+})
