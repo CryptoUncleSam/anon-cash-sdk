0e5b42e4dd4dd6f7b605fdf606ba74132cbcb5d8
stanNthe5 <st@jcow.net>
2025-11-09 09:48:55 +0800

    mv RPC to .env

--- __tests__/e2espl.test.ts
+++ __tests__/e2espl.test.ts
@@ -7,18 +7,21 @@ import { Connection } from "@solana/web3.js";
 dotenv.config();
 const TEST_AMOUNT = 2
 const units_per_token = 1_000_000
-let rpc = "https://domini-i2gp2o-fast-devnet.helius-rpc.com"
 describe('e2e test', async () => {
-    const connection = new Connection(rpc)
-    let fee_recipient_ata = await getAssociatedTokenAddress(USDC_MINT, FEE_RECIPIENT, true)
-    let feeRecipientAccount = await getAccount(connection, fee_recipient_ata)
-    let feeREcipientBalance_before = feeRecipientAccount.amount
-
     if (!process.env.PRIVATE_KEY) {
         throw new Error('missing PRIVATE_KEY in .env')
     }
+    if (!process.env.RPC_URL) {
+        throw new Error('missing RPC_URL in .env')
+    }
+
+    const connection = new Connection(process.env.RPC_URL)
+    let fee_recipient_ata = await getAssociatedTokenAddress(USDC_MINT, FEE_RECIPIENT, true)
+    let feeRecipientAccount = await getAccount(connection, fee_recipient_ata)
+    let feeRecipientBalance_before = feeRecipientAccount.amount
+
     let client = new PrivacyCash({
-        RPC_url: rpc,
+        RPC_url: process.env.RPC_URL,
         owner: process.env.PRIVATE_KEY
     })
     let balance_original = await client.getPrivateBalanceUSDC()
@@ -44,7 +47,7 @@ describe('e2e test', async () => {
     await new Promise(r => setTimeout(r, 10_000));
 
     feeRecipientAccount = await getAccount(connection, fee_recipient_ata)
-    let feeREcipientBalance_after = feeRecipientAccount.amount
+    let feeRecipientBalance_after = feeRecipientAccount.amount
 
     let balance_after_withdraw = await client.getPrivateBalanceUSDC()
 
@@ -65,6 +68,6 @@ describe('e2e test', async () => {
     })
 
     it('fee recipient amount should be inceased by the withdraw fee', () => {
-        expect(withdrawRes.fee_base_units).equal(Number(feeREcipientBalance_after - feeREcipientBalance_before))
+        expect(withdrawRes.fee_base_units).equal(Number(feeRecipientBalance_after - feeRecipientBalance_before))
     })
 })
